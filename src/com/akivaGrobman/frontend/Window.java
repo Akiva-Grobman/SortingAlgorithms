f1c3be5e681883c0937ee204747ce4a197b88d84
akivav <akiva.grobman@gmail.com>
2020-07-20 13:00:21 +0300

    finished Window documentation

--- src/com/akivaGrobman/frontend/Window.java
+++ src/com/akivaGrobman/frontend/Window.java
@@ -14,29 +14,34 @@ public class Window extends JFrame {
     private static final String welcomeDisplayKey = WelcomeDisplay.class.getSimpleName();
     private final SortingDisplay[] sortingDisplays;
     private final SortingDisplayInformation[] sortingDisplayInformation;
-    private final JPanel panelCont;
+    private final JPanel mainDisplayPanel;
     private final CardLayout cardLayout;
 
     public Window(int numberOfAlgorithms) {
+        // will open the window to the full screen size
         setExtendedState(JFrame.MAXIMIZED_BOTH);
+        // will set the window title
         setTitle("Sorting visualizer - By Akiva Grobman");
+        // will make the x button close the window and stop the program
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // when the window is minimized this will be the default size(the smallest size that the opening display still looks normal)
         setMinimumSize(new Dimension(800, 438));
+        // will locate the window in the middle of the screen when minimized
         setLocationRelativeTo(null);
         // we use the card layout so we can switch the panel on display
-        panelCont = new JPanel();
+        mainDisplayPanel = new JPanel();
         cardLayout = new CardLayout();
-        panelCont.setLayout(cardLayout);
+        mainDisplayPanel.setLayout(cardLayout);
+        // will add the welcome display to the mainDisplayPanel (and by default will be the first visible)
         WelcomeDisplay welcomeDisplay = new WelcomeDisplay();
-        panelCont.add(welcomeDisplay, welcomeDisplayKey);
-
+        mainDisplayPanel.add(welcomeDisplay, welcomeDisplayKey);
         // set display panel(that will contain all of the display panels
-        JPanel panel = new JPanel();
-        panel.setBackground(green);
+        JPanel displayPanel = new JPanel();
+        displayPanel.setBackground(green);
         GridLayout sortingDisplayLayout = new GridLayout(getLayoutRowCount(numberOfAlgorithms), getLayoutColumnCount(numberOfAlgorithms));
-        panel.setLayout(sortingDisplayLayout);
+        displayPanel.setLayout(sortingDisplayLayout);
+        // will determent the preferred panel size for when the window is minimized(based on the number of elements and full screen size
         Dimension preferredDisplayPanelSize = new Dimension(getWidth() / sortingDisplayLayout.getColumns(), getHeight() / sortingDisplayLayout.getRows());
-
         // initialize display panels and display info objects
         sortingDisplays = new SortingDisplay[numberOfAlgorithms];
         sortingDisplayInformation = new SortingDisplayInformation[numberOfAlgorithms];
@@ -44,17 +49,21 @@ public class Window extends JFrame {
             sortingDisplayInformation[i] = new SortingDisplayInformation(Main.getSortName(i));
             sortingDisplays[i] = new SortingDisplay(sortingDisplayInformation[i], preferredDisplayPanelSize);
         }
-
-        // add display panels to panel
+        // add display panels to displayPanel
         for (SortingDisplay sortingDisplay : sortingDisplays) {
             sortingDisplay.setBorder(BorderFactory.createLineBorder(blue));
-            panel.add(sortingDisplay);
+            displayPanel.add(sortingDisplay);
         }
-
-        panelCont.add(panel, sortingDisplayKey);
-        add(panelCont);
+        mainDisplayPanel.add(displayPanel, sortingDisplayKey);
+        add(mainDisplayPanel);
         setVisible(true);
     }
+
+    /*
+     *  The next four methods take an index parameter.
+     *  This is so we don't need to update the whole window
+     *  We only want to update the one that had it's date has changed.
+     */
 
     // simple setter that will call the refresh method after updating the list of elements
     public void updateList(List<Integer> list, int index) {
@@ -78,7 +87,7 @@ public class Window extends JFrame {
 
     // will set the sorting display panel to the on visible
     public void replacePanels() {
-        cardLayout.show(panelCont, sortingDisplayKey);
+        cardLayout.show(mainDisplayPanel, sortingDisplayKey);
     }
 
     // these two methods keep this "matrix" as geometrical as possible
